bcfbf966e09d063d3237b0d2b634388cc9bdad55
Vov2002 <Vladimiy2002@mail.ru>
2023-01-18 18:46:34 +0300

    myCommit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// let myBirthDay= '12.07.2002'
+// console.log(myBirthDay5)
+// console.log("Добро пожаловать на курс Владимир")
+// alert("18.01.2023")
+document.getElementById('one').innerHTML='Hello World'
+
+//1 тип данных число
+console.log(Infinity)//infinity
+console.log(3/0)//infinity
+console.log(NaN)//Nan
+console.log('Vladimir' - 2002)//Nan
+
+
+//2 тип данных bigInt
+let bi = 999999999999999999999999999999999999999999n
+console.log(bi)
+
+
+//3 тип данных строка(string)
+let str1 = 'grg'
+let str2 = "jyj"
+let str3 = `krk ${str2}`
+console.log(str1)
+console.log(str2)
+console.log(str3)
+
+
+//4 тип данных BOOlean
+let qtrue = true;
+let qfalse = false;
+let a = 2>1
+let a2 = 2<1
+console.log(qtrue)
+console.log(qfalse)
+console.log(a)
+console.log(a2)
+console.log (false == 0)
+
+//5 тип данных Null
+let nuller = null
+console.log(nuller)
+
+// 6 тип данных underfined
+let test
+console.log(test)
+
+
+//7 тип данных объекты
+
+//8 тип данных typeof
+
+console.log(typeof underfined)
+console.log(typeof 0)
+console.log(typeof 999999n)
+console.log(typeof null)
+console.log(typeof true)
+console.log(typeof 'dog')
+console.log(typeof Symbol ('id'))
+
